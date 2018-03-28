fc846599f3141ce9bb27fe7373482e16fbbfbd1d
Timilsina <arbint@130-199-23-133.dhcp.bnl.gov>
2018-03-28 12:09:59 -0400

    Major updates

--- work/runMakeMapSCE.sh
+++
@@ -1,5 +0,0 @@
-rm -rf MakeMapSCE
-find ../HistoDirectory/ -name "*.pdf" | xargs rm
-rm -rf ../OutputFiles/Result_*
-g++ -Wall -std=c++11 MakeMapSCE.C ../src/MapSCE.C `root-config --cflags --glibs` -o MakeMapSCE;
-./MakeMapSCE
